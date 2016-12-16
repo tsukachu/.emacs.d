5b698445bd79dadc354a91ca0616f1643b6780d7
tsukachu <concon.ch.1543@gmail.com>
2016-12-16 12:30:20 +0900

    Add ddskk's keybind

--- lisp/99_global-keys.el
+++ lisp/99_global-keys.el
@@ -1,5 +1,7 @@
 (bind-key* "C-h" 'delete-backward-char)
 
+(bind-key* "C-x j" 'skk-mode)
+
 (bind-key* "C-x C-f" 'helm-find-files)
 (bind-key* "C-x b" 'helm-buffers-list)
 (bind-key* "M-x" 'helm-M-x)
